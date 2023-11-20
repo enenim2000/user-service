444249013493a6caf91218c4df0e2f25202ca1de
Enenim Bassey <enenim2000@gmail.com>
2023-11-20 08:25:00 +0100

    initial commit

---
+++ src/main/java/com/elara/userservice/auth/controller/AuthController.java
@@ -0,0 +1,42 @@
+package com.elara.userservice.auth.controller;
+
+
+import com.elara.userservice.auth.model.AuthRequest;
+import com.elara.userservice.auth.model.AuthResponse;
+import com.elara.userservice.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody AuthRequest authRequest) {
+        return ResponseEntity.ok(authService.register(authRequest));
+    }
+
+    @PostMapping(value = "/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) {
+        return ResponseEntity.ok(authService.register(authRequest));
+    }
+
+    @GetMapping(value = "/logout")
+    public ResponseEntity<AuthResponse> logout() {
+        return ResponseEntity.ok(AuthResponse.builder()
+                        .accessToken("token")
+                        .refreshToken("refresh")
+                        .build());
+    }
+
+    @PostMapping(value = "/roles")
+    public ResponseEntity<AuthResponse> createUserRoles() {
+        return ResponseEntity.ok(AuthResponse.builder()
+                .accessToken("roles")
+                .refreshToken("created")
+                .build());
+    }
+}
---
+++ src/main/java/com/elara/userservice/auth/service/AuthService.java
@@ -0,0 +1,20 @@
+package com.elara.userservice.auth.service;
+
+import com.elara.userservice.auth.model.AuthRequest;
+import com.elara.userservice.auth.model.AuthResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final JwtService jwtService;
+
+    public AuthResponse register(AuthRequest authRequest) {
+        String accessToken = jwtService.generate(authRequest.getEmail(), "ACCESS");
+        String refreshToken = jwtService.generate(authRequest.getEmail(), "REFRESH");
+
+        return new AuthResponse(accessToken, refreshToken);
+    }
+}
